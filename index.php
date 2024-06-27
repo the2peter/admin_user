e1e71d1dd2b240e7852f26756cd43a7d4e309f30
the2peter <89401709+the2peter@users.noreply.github.com>
2024-06-27 12:37:34 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php include('condb.php');?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css&quot; integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css&quot; integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <title>Login by.devtai.com</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container" style="padding-top:100px">
+  <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4" style="background-color:#D6EAF8">
+      <h3 align="center">
+      <span class="glyphicon glyphicon-lock"> </span>
+      Form Login </h3>
+      <form  name="formlogin" action="checklogin.php" method="POST" id="login" class="form-horizontal">
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input type="text"  name="username" class="form-control" required placeholder="Username" />
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input type="password" name="password" class="form-control" required placeholder="Password" />
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-12">
+            <button type="submit" class="btn btn-success" id="btn">
+            <span class="glyphicon glyphicon-log-in"> </span>
+             Login </button>
+               <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+               </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+  </body>
+</html>
